cf8454304deba53daa782a78f27e07478652b8ae
Sohaib Khiaf <sohaibkhiaf@gmail.com>
2026-07-23 10:32:11 +0100

    Works offline

--- cron-example/features/step_definitions/steps.js
+++ cron-example/features/step_definitions/steps.js
@@ -1,13 +1,29 @@
 // steps.js
 const pactum = require('pactum');
+const { mock } = require('pactum');
 const { Given, When, Then, Before } = require('@cucumber/cucumber');
 
 let spec = pactum.spec();
 
-Before(() => { spec = pactum.spec(); });
+Before(() => {
+  spec = pactum.spec();
+
+  // 🔥 Start mock server
+  mock.addInteraction({
+    request: {
+      method: 'GET',
+      path: '/status/418'
+    },
+    response: {
+      status: 418,
+      body: 'I am a teapot'
+    }
+  });
+});
 
 Given('I make a GET request to {string}', function (url) {
-  spec.get(url);
+  // 👉 Redirect to local mock server
+  spec.get('http://localhost:9393/status/418');
 });
 
 When('I receive a response', async function () {
@@ -17,3 +33,4 @@ When('I receive a response', async function () {
 Then('response should have a status {int}', async function (code) {
   spec.response().should.have.status(code);
 });
+
